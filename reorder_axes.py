c853ae1a9572381cf8d14d1b8389fc58881b4632
Fabio Echegaray <Fabio.EchegarayIturra@ucsf.edu>
2022-03-01 10:52:00 -0800

    Fix the metadata information that was being saved on tif files in the reorder_axes.py script.

--- reorder_axes.py
+++ reorder_axes.py
@@ -1,15 +1,16 @@
+import sys
 import itertools
 from math import floor
+
+import numpy as np
 
 import javabridge
 import bioformats
 import bioformats as bf
-import ome_types
-from ome_types.model import Channel
+import bioformats.omexml as ome
 import tifffile as tf
 from bioformats.formatreader import ImageReader
 from javabridge import JavaException
-from ome_types.model.pixels import DimensionOrder
 from tifffile import tiffcomment
 
 
@@ -31,12 +32,34 @@ def silence_javabridge_log():
                     log_level)
 
 
-def image_it(frames_per_block, folder, n_files=0, position=0, n_positions=1):
+def writeplanes(pixel, SizeT=1, SizeZ=1, SizeC=1, order='TZCYX', verbose=False):
+    if order == 'TZCYX':
+        counter = 0
+        for t in range(SizeT):
+            for z in range(SizeZ):
+                for c in range(SizeC):
+
+                    if verbose:
+                        print('Write PlaneTable: ', t, z, c),
+                        sys.stdout.flush()
+
+                    pixel.Plane(counter).TheT = t
+                    pixel.Plane(counter).TheZ = z
+                    pixel.Plane(counter).TheC = c
+                    counter = counter + 1
+
+    return pixel
+
+
+def image_it(frames_per_block, pre_fname, folder, n_files=0, position=0, n_positions=1):
     print(f'image_it n_files={n_files}')
+
+    def fname(i):
+        return folder + f'{pre_fname}{"_" + str(i) if i > 0 else ""}.ome.tif'
+
     frames_of_all_files = 0
     for i in range(n_files):
-        fname = folder + f'plate_1_real_experiment_1_MMStack{"_" + str(i) if i >= 0 else ""}.ome.tif'
-        o = bf.OMEXML(bf.get_omexml_metadata(path=fname))
+        o = bf.OMEXML(bf.get_omexml_metadata(path=fname(i)))
         im = o.image(index=0)
         frames_of_all_files += im.Pixels.SizeT
 
@@ -45,19 +68,19 @@ def image_it(frames_per_block, folder, n_files=0, position=0, n_positions=1):
                    for i in range(n_blocks)
                    if i % n_positions == position]
     img_ix_list = list(itertools.chain(*img_ix_list))
+    print(img_ix_list)
 
     img_count = 0
     for i in range(n_files):
-        fname = folder + f'plate_1_real_experiment_1_MMStack{"_" + str(i) if i >= 0 else ""}.ome.tif'
-        print(f"file {fname}")
-        with ImageReader(fname, perform_init=True) as reader:
+        print(f"file {fname(i)}")
+        with ImageReader(fname(i), perform_init=True) as reader:
             # re-read metadata for the specific file
-            o = bf.OMEXML(bf.get_omexml_metadata(path=fname))
+            o = bf.OMEXML(bf.get_omexml_metadata(path=fname(i)))
             im = o.image(index=0)
             for frame in range(im.Pixels.SizeT):
-                img_count += 1
                 should_yield = img_count in img_ix_list
                 print(f'frame {frame} img_count {img_count} {should_yield}')
+                img_count += 1
                 if should_yield:
                     image = reader.read(c=0, z=0, t=frame, rescale=False)
                     yield image
@@ -67,8 +90,23 @@ if __name__ == "__main__":
     javabridge.start_vm(class_path=bioformats.JARS, run_headless=True)
     silence_javabridge_log()
 
-    folder = '/media/lab/Data/Andrew/temp_data/plate_1_real_experiment_1/ome-tif_files/'
-    fname = folder + 'plate_1_real_experiment_1_MMStack_0.ome.tif'
+    # parameters of modular arithmetic
+    # to get the correct images of a particular specimen position
+    position = 0
+    n_zstacks = 1
+    n_positions = 9
+    n_channels = 1
+    zc = n_zstacks * n_channels
+    gap = n_positions * zc
+
+    folder = '/media/lab/Data/Fabio/Nikon/20220117 - SFWT UE/no_treatment_3/'
+    pre_fname = 'no_treatment_3_MMStack'
+    fname = f"{folder}{pre_fname}.ome.tif"
+    # fname = '/media/lab/Data/Fabio/Nikon/20220117 - SFWT UE/no_treatment_3_MMStack_pos0.ome.tiff'
+
+    # initialize image writer
+    out_file = f"{pre_fname}{position}.ome.tif"
+    tif = tf.TiffWriter(out_file, imagej=False, bigtiff=True)
 
     # read image metadata
     o = bf.OMEXML(bf.get_omexml_metadata(path=fname))
@@ -82,45 +120,63 @@ if __name__ == "__main__":
           f"{im.Pixels.SizeX} px on X dir, {im.Pixels.SizeY} px on Y dir, {im.Pixels.SizeZ} px on Z dir.")
     assert im.Pixels.SizeC == 1 and im.Pixels.SizeZ == 1
 
-    # using modular arithmetic to get the correct images of a particular specimen position
-    position = 0
-    n_zstacks = 51
-    n_positions = 7
-    n_channels = 2
-    zc = n_zstacks * n_channels
-    gap = n_positions * zc
-
-    # initialize image writer
-    out_file = f"plate_1_real_experiment_1_MMStack_pos{position}.ome.tiff"
-    writer = tf.TiffWriter(out_file, imagej=True, bigtiff=True)
-
     # iterate through all files and get the images of specific position
     pos_frame = 0
-    for it_frame, image in enumerate(image_it(zc, folder, n_files=1, position=position, n_positions=n_positions)):
-        writer.save(image, contiguous=True, photometric='minisblack', metadata={'channels': n_channels})
-        # bf.write_image(out_file, image, pixel_type='uint16', c=floor(it_frame / n_zstacks), z=it_frame % n_zstacks,
-        #                t=pos_frame, size_c=n_channels, size_z=n_zstacks)
+    for it_frame, image in enumerate(
+            image_it(zc, pre_fname, folder, n_files=154, position=position, n_positions=n_positions)):
+        tif.save(image, contiguous=False, photometric='minisblack', metadata={})
         if it_frame % zc == 0:
             pos_frame += 1
         print(f"it_frame -> {it_frame} pos_frame -> {pos_frame}")
 
-    writer.close()
+    tif.close()
 
     # update metadata
     print(f'pos_frame {pos_frame} n_channels {n_channels} n_zstacks {n_zstacks}')
-    fname = folder + 'plate_1_real_experiment_1_MMStack_0.ome.tif'
-    ome = ome_types.from_xml(bf.get_omexml_metadata(path=fname))
-    ome.images[0].pixels.dimension_order = DimensionOrder.XYZCT
-    ome.images[0].pixels.size_t = pos_frame
-    ome.images[0].pixels.size_c = n_channels
-    ome.images[0].pixels.size_z = n_zstacks
-    # ch_2 = deepcopy(ome.images[0].pixels.channels[0])
-    # ch_2['id'] = 'Channel:0:1'
-    ome.images[0].pixels.channels.append(Channel())
+    fname = folder + 'no_treatment_3_MMStack.ome.tif'
+
+    SizeX = im.Pixels.SizeX
+    SizeY = im.Pixels.SizeY
+    SizeC = n_channels
+    SizeT = pos_frame
+    SizeZ = n_zstacks
+    scalex = im.Pixels.PhysicalSizeX
+    scaley = im.Pixels.PhysicalSizeY
+    scalez = im.Pixels.PhysicalSizeZ
+    Series = 0
+    pixeltype = 'uint16'
+    dimorder = 'TZCYX'
+
+    # Create metadata info
+    omexml = ome.OMEXML()
+    omexml.image(Series).Name = out_file
+    p = omexml.image(Series).Pixels
+    # p.ID = 0
+    p.DimensionOrder = im.Pixels.DimensionOrder
+    p.SizeX = SizeX
+    p.SizeY = SizeY
+    p.SizeC = SizeC
+    p.SizeT = SizeT
+    p.SizeZ = SizeZ
+    p.PhysicalSizeX = float(scalex)
+    p.PhysicalSizeY = float(scaley)
+    p.PhysicalSizeZ = float(scalez if scalez else 0)
+    p.PixelType = pixeltype
+    p.channel_count = SizeC
+    p.plane_count = SizeZ * SizeT * SizeC
+    p = writeplanes(p, SizeT=SizeT, SizeZ=SizeZ, SizeC=SizeC, order=dimorder)
+
+    for c in range(SizeC):
+        if pixeltype == 'unit8':
+            p.Channel(c).SamplesPerPixel = 1
+        if pixeltype == 'unit16':
+            p.Channel(c).SamplesPerPixel = 2
+
+    omexml.structured_annotations.add_original_metadata(
+        ome.OM_SAMPLES_PER_PIXEL, str(SizeC))
+
     # ome.images[0].description = 'Image 0 description'
-    tiffcomment(out_file, ome.to_xml().encode('utf-8'))
-    with open('ome.xml', mode='w') as f:
-        f.write(ome.to_xml())
+    tiffcomment(out_file, omexml.to_xml().encode('utf-8'))
 
     print('Done writing image.')
     javabridge.kill_vm()
