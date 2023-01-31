f1840be9dc938f0e84c5d35b57de4089b8daa610
Fabio Echegaray <Fabio.EchegarayIturra@ucsf.edu>
2023-01-30 18:59:16 -0800

    Now ensure_dir returns a Path object consistently to the input.

--- fileops/pathutils.py
+++ fileops/pathutils.py
@@ -4,7 +4,8 @@ from typing import Union
 
 
 def ensure_dir(dir_path: Union[str, Path]):
-    dir_path = os.path.abspath(dir_path)
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path, exist_ok=True)
-    return dir_path
+    is_path = type(dir_path) == Path
+    adir_path = os.path.abspath(dir_path)
+    if not os.path.exists(adir_path):
+        os.makedirs(adir_path, exist_ok=True)
+    return Path(dir_path) if is_path else dir_path
