74464659c6b1a1fe66789226f0557e03009979fe
Fabio Echegaray <Fabio.EchegarayIturra@ucsf.edu>
2023-08-08 14:36:37 -0700

    Add parameter to bioformats_to_tiffseries that restricts the number of frames to export. Manages some edge cases when reading files in bioformats_to_tiffseries.

--- fileops/export/_bioformats.py
+++ fileops/export/_bioformats.py
@@ -1,6 +1,6 @@
 import os.path
 from pathlib import Path
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 import numpy as np
 from tifffile import imwrite
@@ -13,36 +13,44 @@ from fileops.logger import get_logger
 log = get_logger(name='export')
 
 
-def bioformats_to_tiffseries(img_struct: ImageFile, save_path=Path('_vol_paraview')) -> Dict:
+def bioformats_to_tiffseries(img_struct: ImageFile, save_path=Path('_vol_paraview'), until_frame=np.inf) -> Tuple[
+    np.array, Dict]:
     log.info("Exporting bioformats file to series of tiff file volumes.")
     save_path = ensure_dir(save_path)
 
-    out = dict()
+    dct = dict()
     image = np.empty(shape=(len(img_struct.zstacks), *img_struct.image(0).image.shape), dtype=np.uint16)
     for j, c in enumerate(img_struct.channels):
-        out[f"ch{c:01d}"] = {
+        dct[f"ch{c:01d}"] = {
             "files":  [],
             "minmax": []
         }
         ensure_dir(save_path / f"ch{c:01d}")
         for fr in img_struct.frames:
+            if fr > until_frame:
+                break
+
             fname = f'C{c:02d}T{fr:04d}_vol.tiff'
             fpath = save_path / f"ch{c:01d}" / fname
 
-            out[f"ch{c:01d}"]["files"].append(fpath.as_posix())
-            out[f"ch{c:01d}"]["minmax"].append((np.min(image), np.max(image)))
+            dct[f"ch{c:01d}"]["files"].append(fpath.as_posix())
+            dct[f"ch{c:01d}"]["minmax"].append((np.min(image), np.max(image)))
 
-            log.debug(f"Saving image {fname} in cache (path={fpath}).")
+            log.debug(f"Attempting to save image {fname} in path={fpath}.")
             if not os.path.exists(fpath):
-                for i, z in enumerate(img_struct.zstacks):
-                    ix = img_struct.ix_at(c=j, z=z, t=fr)
-                    mdimg = img_struct.image(ix)
-                    image[i, :, :] = mdimg.image
-                imwrite(fpath, np.array(image), imagej=True, metadata={'order': 'ZXY'})
+                try:
+                    for i, z in enumerate(img_struct.zstacks):
+                        ix = img_struct.ix_at(c=j, z=z, t=fr)
+                        mdimg = img_struct.image(ix)
+                        if mdimg and hasattr(mdimg, "image") and mdimg.image is not None:
+                            image[i, :, :] = mdimg.image
+                    imwrite(fpath, np.array(image), imagej=True, metadata={'order': 'ZXY'})
+                except FrameNotFoundError or IndexError:
+                    print(f"Frame {fr} not found (file corrupted?)")
             else:
                 print(f"skipping file {fpath.as_posix()}")
 
-    return out
+    return image, dct
 
 
 def bioformats_to_ndarray_zstack(img_struct: OMEImageFile, roi=None, channel=0, frame=0):
