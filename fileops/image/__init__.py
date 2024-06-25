d8906d2df74948ab18d4b9c0b1878e323b1746bd
Fabio Echegaray <Fabio.EchegarayIturra@ucsf.edu>
2024-06-25 14:48:17 -0700

    Add PycroManagerSingleImageStack to __init__.py of image sub-package.

--- fileops/image/__init__.py
+++ fileops/image/__init__.py
@@ -2,6 +2,7 @@ from ._image_file_tiffile_ome import OMEImageFile
 from ._mmagellan import folder_is_micromagellan
 from ._mmanager_folder_series import MicroManagerFolderSeries
 from ._mmanager_single_stack import MicroManagerSingleImageStack
+from ._pycromanager_single_stack import PycroManagerSingleImageStack
 from .image_file import ImageFile
 from .imagemeta import MetadataImage, MetadataImageSeries
 from .to_8bit import to_8bit
