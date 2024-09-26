59b3e62a91d1a8ceb29ae349e5b17cb523c4138f
Fabio Echegaray <fabio.echegaray@gmail.com>
2024-09-26 12:30:20 -0700

    Modify template to make it more readable and tweak some parameters involving color transfer functions.

--- fileops/export/_openvdb.py
+++ fileops/export/_openvdb.py
@@ -14,9 +14,9 @@ log = get_logger(name='export-openvdb')
 def export_paraview(cfg: ExportConfig, out_path: Path, until_frame=np.inf):
     log.info(f"Exporting data from configuration file {cfg.path} into Paraview format")
 
-    red_trans_fn = "[0, 0.0, 0.0, 0.0, 12000, 1.0, 0.0, 0.0]"
-    grn_trans_fn = "[0, 0.0, 0.0, 0.0, 12000, 0.0, 1.0, 0.0]"
-    blu_trans_fn = "[0, 0.0, 0.0, 0.0, 12000, 0.0, 0.0, 1.0]"
+    red_trans_fn = "[0, 0.0, 0.0, 0.0, 4000, 1.0, 0.0, 0.0, 9000, 1.0, 0.0, 0.0]"
+    grn_trans_fn = "[0, 0.0, 0.0, 0.0, 4000, 0.0, 1.0, 0.0, 9000, 0.0, 1.0, 0.0]"
+    blu_trans_fn = "[0, 0.0, 0.0, 0.0, 4000, 0.0, 0.0, 1.0, 9000, 0.0, 0.0, 1.0]"
     tfn_lst = [red_trans_fn, grn_trans_fn, blu_trans_fn]
 
     channels = dict()
@@ -27,6 +27,9 @@ def export_paraview(cfg: ExportConfig, out_path: Path, until_frame=np.inf):
     dtype_max = np.iinfo(np.uint16).max
     for chkey, tr_fn in zip(ch_metadata.keys(), tfn_lst):
         ch = int(chkey[2:])
+        x1 = int((ch_metadata[chkey]["mean"][0] - ch_metadata[chkey]["std"][0]) / 100) * 100
+        x2 = int(ch_metadata[chkey]["mean"][0] / 100) * 100
+        x3 = int((ch_metadata[chkey]["mean"][0] + ch_metadata[chkey]["std"][0]) / 100) * 100
         channels[chkey] = {
             "label":               f"ch{ch:01d}",
             # mind the folder structure to get these names right
@@ -34,13 +37,14 @@ def export_paraview(cfg: ExportConfig, out_path: Path, until_frame=np.inf):
             "session":             cfg.image_file.image_path.parent.parent.name,
             "tiff_files_list":     str(ch_metadata[chkey]["files"]),
             "ctf_rgb_points":      tr_fn,
-            "otf_opacity_points":  "[0,    0.000, 0.5, 0.0, "
-                                   "2000,  0.000, 0.5, 0.0, "
-                                   "6500,  0.030, 0.5, 0.0, "
-                                   "9300,  0.200, 0.5, 0.0, "
-                                   "13000, 0.200, 0.5, 0.0]",
-            "scale_transfer_fn":   "[0, 0.0, 0.5, 0.0, 11670, 1.0, 0.5, 0.0]",
-            "opacity_transfer_fn": "[0, 0.0, 0.5, 0.0, 11670, 1.0, 0.5, 0.0]",
+            "otf_opacity_points":  "[\n"
+                                   "0    , 0.000, 0.5, 0.0,\n"
+                                   f"{x1:<5}, 0.000, 0.5, 0.0,\n"
+                                   f"{x2:<5}, 0.000, 0.5, 0.0,\n"
+                                   f"{x3:<5}, 0.120, 0.5, 0.0,\n"
+                                   "8000,  0.120, 0.5, 0.0]",
+            "scale_transfer_fn":   f"[0, 0.0, 0.5, 0.0, {x1}, 1.0, 0.5, 0.0]",
+            "opacity_transfer_fn": f"[0, 0.0, 0.5, 0.0, {x1}, 1.0, 0.5, 0.0]",
             "exp_corr_params":     ch_metadata[chkey]["photobleach_params"],
             "mean":                ch_metadata[chkey]["mean"],
             "minmax":              ch_metadata[chkey]["minmax"],
@@ -48,4 +52,7 @@ def export_paraview(cfg: ExportConfig, out_path: Path, until_frame=np.inf):
             "max":                 np.max([mm[1] for mm in ch_metadata[chkey]["minmax"]]),
             "dtype_max":           dtype_max
         }
-    save_vtk_python_state(out_path / f"paraview_state.py", channel_info=channels)
+        ginfo={
+            "frames": cfg.image_file.n_frames,
+        }
+    save_vtk_python_state(out_path / f"paraview_state.py", general_info=ginfo, channel_info=channels)
