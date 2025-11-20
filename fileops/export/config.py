854c97aa80d93617fea49cda1005b425ef807ccc
Fabio Echegaray <fabio.echegaray@gmail.com>
2025-11-20 14:52:13 -0500

    Now the header DATA is mandatory in the definition of a configuration file. Failure to include it renders a syntax error.

--- fileops/export/config.py
+++ fileops/export/config.py
@@ -161,19 +161,15 @@ def _read_data_section(cfg_path):
     return cfg, img_file, param_override, roi
 
 
-def read_config(cfg_path) -> ExportConfig:
+def read_config(cfg_path: Path) -> ExportConfig:
+    cfg_path = cfg_path.absolute()
+    if not cfg_path.exists():
+        raise FileNotFoundError
     cfg = configparser.ConfigParser()
     cfg.read(cfg_path)
 
     if "DATA" not in cfg:
-        log.error(f"No header DATA in file {cfg_path}.")
-        return ExportConfig(
-            config_file=cfg,
-            path=None,
-            name=None,
-            movies=[],
-            panels=[]
-        )
+        raise SyntaxError(f"No header DATA in file {cfg_path}.")
 
     cfg_movie = read_config_movie(cfg_path)
     cfg_panel = read_config_panel(cfg_path)
@@ -356,6 +352,7 @@ def build_config_list(ini_path: Path) -> pd.DataFrame:
             "img_fld":      Path(cfg["DATA"]["image"]).parent.name,
             "title":        cfg["MOVIE"]["title"],
             "description":  cfg["MOVIE"]["description"],
+            "bitrate":      cfg["MOVIE"]["bitrate"] if "bitrate" in cfg["MOVIE"] else "500k",
             "t_collection": col_m,
             "t_incubation": inc_m,
             "fps":          cfg["MOVIE"]["fps"] if "fps" in cfg["MOVIE"] else 10,
