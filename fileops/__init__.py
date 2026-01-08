adc5b0aa0ec9afdd97bbfc2e9311ca4afea2438e
Fabio Echegaray <fabio.echegaray@gmail.com>
2026-01-08 10:55:21 -0500

    Add the base class of a plugin system. Implement plugin discovery using importlib at the loading of the package.

---
+++ fileops/__init__.py
@@ -0,0 +1,19 @@
+import sys
+
+from fileops.logger import get_logger
+
+# check for plugins
+if sys.version_info < (3, 10):
+    from importlib_metadata import entry_points
+else:
+    from importlib.metadata import entry_points
+
+log = get_logger(name='plugin-engine')
+
+# load all types
+config_type_plugins = entry_points(group='fileops.plugins.config.types')
+for dp in config_type_plugins:
+    log.info(f"found plugin for header {dp.name} ({dp.value})")
+
+header_reader_plugins = entry_points(group='fileops.plugins.config.header_readers')
+log.info(f"Found {len(header_reader_plugins)} configuration section parsers.")
