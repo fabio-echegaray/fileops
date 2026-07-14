7129f281408e754d76f8ce78d0630027a2955191
Fabio Echegaray <fabio.echegaray@gmail.com>
2026-07-14 16:26:52 -0400

    Add function rescale into fileops.image.ops.

--- fileops/image/ops/__init__.py
+++ fileops/image/ops/__init__.py
@@ -1,3 +1,4 @@
 from ._bleach_correction import photobleach_fit, bleach_func
+from ._image_rescale import rescale
 from ._z_projection import z_projection
 from ._z_projection_types import ZProjection, zprojection_from_str
