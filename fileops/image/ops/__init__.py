92ccaceacd918c06120fb51fc0abbed160c45fda
Fabio Echegaray <fabio.echegaray@gmail.com>
2026-07-18 10:42:57 -0400

    Implement image rescale as an ImageProcessor.

--- fileops/image/ops/__init__.py
+++ fileops/image/ops/__init__.py
@@ -1,5 +1,5 @@
 from ._bleach_correction import photobleach_fit, bleach_func
-from ._image_rescale import rescale
+from ._image_rescale import rescale, normalize_to_dtype
 from ._img2D_histogram_matching import image_match_histograms
 from ._z_projection import z_projection
 from ._z_projection_types import ZProjection, zprojection_from_str
